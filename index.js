928ab9b5cede3363bcbaad8dfdf26af893885b85
TarasPyts <taras.a.pyts@gmail.com>
2023-01-03 11:31:14 +0200

    new project

---
+++ index.js
@@ -0,0 +1,19 @@
+const txt = document.getElementById('textAdd');
+const out1 = document.getElementById('output1');
+const out2 = document.getElementById('output2');
+const out3 = document.getElementById('output3');
+
+
+
+const createTask = () => {
+    const value = document.getElementById("select").value;
+    const outText = document. createElement('p')
+    outText.textContent = txt.value;
+    if (value == 'column1') {
+    out1.appendChild(outText)
+    } else if (value == 'column2'){
+    out2.appendChild(outText)
+    } else {
+    out3.appendChild(outText)
+    }
+}
