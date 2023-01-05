68fc1e0eb543df3817ccc3d77702dcd968f6b85c
TarasPyts <taras.a.pyts@gmail.com>
2023-01-05 18:54:54 +0200

    content changes

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const out1 = document.getElementById('output1');
 const out2 = document.getElementById('output2');
 const out3 = document.getElementById('output3');
 
-
-
 const createTask = () => {
     const value = document.getElementById("select").value;
-    const outText = document. createElement('p')
+    const outText = document.createElement('p');
+    
     outText.textContent = txt.value;
     if (value == 'column1') {
-    out1.appendChild(outText)
+        out1.appendChild(outText)
     } else if (value == 'column2'){
-    out2.appendChild(outText)
+        out2.appendChild(outText)
     } else {
-    out3.appendChild(outText)
+        out3.appendChild(outText)
     }
 }
